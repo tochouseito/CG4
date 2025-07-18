70a4a8f67fd4ec40eafe953f47242196496c90ca
tochouseito <146930194+tochouseito@users.noreply.github.com>
2025-07-18 12:58:47 +0900

    シェーダー修正

--- project/Engine/base/GraphicsPipelineState.cpp
+++ project/Engine/base/GraphicsPipelineState.cpp
@@ -2519,50 +2519,6 @@ IDxcBlob* GraphicsPipelineState::CompilerShader(
 	IDxcCompiler3* dxcCompiler,
 	IDxcIncludeHandler* includeHandler)
 {
-	//// hlslファイルを読む
-	//// これからシェーダーをコンパイルする旨をログに出す
-	//Log(ConvertString(std::format(L"Begin CompilerShader,path:{},profile:{}\n", filePath, profile)));
-	//// hlslファイルを読む
-	//IDxcBlobEncoding* shaderSource = nullptr;
-	//HRESULT hr = dxcUtils->LoadFile(filePath.c_str(), nullptr, &shaderSource);
-	//// 読めなかったら止める
-	//assert(SUCCEEDED(hr));
-	//// 読み込んだファイルの内容を設定する
-	//DxcBuffer shaderSourceBuffer;
-	//shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
-	//shaderSourceBuffer.Size = shaderSource->GetBufferSize();
-	//shaderSourceBuffer.Encoding = DXC_CP_UTF8;// UTF8の文字コードであることを通知
-	//LPCWSTR arguments[] = {
-	//	filePath.c_str(),       //コンパイル対象のhlslファイル名
-	//	L"-E",L"main",          // エントリーポイントの指定。基本的にmain以外にはしない
-	//	L"-T",profile,          // ShaderPrifileの設定
-	//	L"-Zi",L"-Qembed_debug",// デバッグ用の情報を埋め込む
-	//	L"-Od",                 // 最適化を外しておく
-	//	L"-Zpr",                // メモリレイアウトは行優先
-	//};
-	//// 実際にShaderをコンパイルする
-	//IDxcResult* shaderResult = nullptr;
-	//hr = dxcCompiler->Compile(
-	//	&shaderSourceBuffer,       // 読み込んだファイル
-	//	arguments,                 // コンパイルオプション
-	//	_countof(arguments),       // コンパイル結果
-	//	includeHandler,            // includeが含まれた諸々
-	//	IID_PPV_ARGS(&shaderResult)// コンパイル結果
-	//);
-	//// コンパイルエラーではなくdxcが起動できないなど致命的な状況
-	//assert(SUCCEEDED(hr));
-
-	//// コンパイル結果から実行用のバイナリ部分を取得
-	//IDxcBlob* shaderBlob = nullptr;
-	//hr = shaderResult->GetOutput(DXC_OUT_OBJECT, IID_PPV_ARGS(&shaderBlob), nullptr);
-	//assert(SUCCEEDED(hr));
-	//// 成功したログを出す
-	//Log(ConvertString(std::format(L"Compile Succeeded,path:{},profile:{}\n", filePath, profile)));
-	//// もう使わないリソースを解放
-	//shaderSource->Release();
-	//shaderResult->Release();
-	//// 実行用のバイナリを返却
-	//return shaderBlob;
 	HRESULT hr = {};
 	Log(ConvertString(std::format(L"Begin CompilerShader,path:{},profile:{}\n", filePath, profile)).c_str());
 	Microsoft::WRL::ComPtr<IDxcBlobEncoding> pSource = nullptr;
